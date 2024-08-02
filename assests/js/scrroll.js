ca47539b538fe93d6a8a42206e111ef097af1eea
ChristopherTelles <teleschristopher@gmail.com>
2024-08-02 00:08:49 -0300

    Mudando o tamanho do WRAPPER/EU SOU e concertando alguns bugs mobile

--- assests/js/scrroll.js
+++ assests/js/scrroll.js
@@ -1,4 +1,29 @@
-const observer = new IntersectionObserver(entries => {
-    console.log(entries)
-})
-observer.observe(document.querySelector(".init-hidden"))
+// Aguarda o carregamento completo do conteúdo do DOM antes de executar o código
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Cria uma instância de IntersectionObserver para monitorar os elementos de destino
+    const observer = new IntersectionObserver(entries => {
+
+        // Itera sobre cada entrada (entry) que o observer está monitorando
+        entries.forEach(entry => {
+
+            // Verifica se o elemento está visível no viewport
+            if (entry.isIntersecting) {
+                // Se o elemento está visível, adiciona a classe para removê-lo do estado inicial oculto
+                entry.target.classList.add('init-hidden-off');
+            } else {
+                // Se o elemento não está visível, remove a classe para voltar ao estado inicial oculto
+                entry.target.classList.remove('init-hidden-off');
+            }
+        });
+    }, {
+        // Define o threshold, indicando a porcentagem de visibilidade necessária para ativar o callback
+        threshold: 0.4 // Quando 50% do elemento está visível, o callback é acionado
+    });
+
+    // Seleciona todos os elementos com a classe 'init-hidden'
+    document.querySelectorAll('.init-hidden').forEach(element => {
+        // Observa cada elemento selecionado, aplicando o IntersectionObserver
+        observer.observe(element);
+    });
+});
